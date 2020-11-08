f224d6baf7a9bb42199e7e947514e5450fad3cb7
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-11-08 18:44:09 +0000

    Output message when spawning select directory dialog

--- sitePowerSetup/getFolder.ps1
+++ sitePowerSetup/getFolder.ps1
@@ -1,6 +1,8 @@
 Function Get-Folder {
     $null = [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
 
+    Write-Host "Spawning site directory dialog, please use alt-tab to find it if not visible"
+    
     $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
     $foldername.rootfolder = "MyComputer"
     $foldername.Description = "Choose local IIS site directory"
